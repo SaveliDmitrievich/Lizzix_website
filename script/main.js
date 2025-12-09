088c43fe60bd566c1e452a927464538d05f79188
saveli <59chelovek59@gmail.com>
2025-12-09 17:40:03 +0300

    Update main.js: add gallery and biography pages, initialize router, refactor home page init functions

--- script/main.js
+++ script/main.js
@@ -6,6 +6,9 @@ import { initSlider } from './components/slider.js'
 import { initCircular } from './components/circular-text.js'
 import { initSliderButtons } from './components/slider.js'
 import { initPagination } from './components/slider.js'
+import { Router } from '../utils/router.js'
+import { gallery } from './pages/gallery.js'
+import { biography } from './pages/biography.js'
 
 function render(root, content) {
 	root.innerHTML = ''
@@ -14,22 +17,41 @@ function render(root, content) {
 
 document.addEventListener('DOMContentLoaded', () => {
 	const main = document.getElementById('main')
-
-	render(main, home())
 	initBurger({
 		burgerBtn: document.getElementById('burger'),
 		closeBtn: document.getElementById('mobile-menu-close'),
 		overlay: document.getElementById('mobile-menu-overlay'),
 		menu: document.getElementById('mobile-menu'),
 	})
-	initStarShooting({ container: document.getElementById('starShooting') })
+	const router = new Router({
+		home: () => {
+			render(main, home())
+			initHome()
+		},
+		gallery: () => {
+			render(main, gallery())
+			// initGallery()
+		},
+		biography: () => {
+			render(main, biography())
+			initBiography()
+		},
+	})
 
+	router.route()
+})
+
+function initHome() {
+	initStarShooting({ container: document.getElementById('starShooting') })
 	initSlider({ sliderId: 'slider', heartId: 'heart-icon' })
 	initHeartAnimation({ container: document.getElementById('heart-icon') })
 	initSliderButtons()
-
 	initPagination()
 	initCircular(document.getElementById('elizabeth-text'), 0.8)
 	initCircular(document.getElementById('pickme-text'), -0.8)
 	initCircular(document.getElementById('creator-text'), 0.8)
-})
+}
+
+function initBiography() {
+	// initStarShooting({ container: document.getElementById('biography-block') })
+}
